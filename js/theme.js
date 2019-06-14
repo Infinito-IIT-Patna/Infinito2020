979cc9266a7f440d71f022f5f93de9582a961900
ritwizsinha <ritwizsinha0@gmail.com>
2019-06-14 21:19:38 +0530

    made the layout of modal

--- js/theme.js
+++ js/theme.js
@@ -331,3 +331,34 @@ jQuery(window).on('load', function () {
       prealoader ()
   })(jQuery);
  });
+
+
+
+ 
+//For the modal
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
