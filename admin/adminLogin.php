f9d9378f1bf0f6317d8ff806db284ed7f7ac92c2
ritwizsinha <ritwizsinha0@gmail.com>
2019-07-02 05:03:49 +0530

    Added the post for scores

--- admin/adminLogin.php
+++ admin/adminLogin.php
@@ -3,33 +3,13 @@
 <?php
 // Enabling Database Connection
 require("../connect.php");
-$USERNAME  = "admin";
-$PASSWORD = "admin";
+require('../details.php');
 $username = $password = '';
 $errors = array('username' => '', 'noData' => '');
 // For checking if the essentials provided by a user for login is correct
 if (isset($_POST["submit-button"])) {
     $username=  trim($_POST['username']);
     $password = $_POST['password'];
-        // Making SQL query to the database
-        // $sql = "SELECT password FROM adminPage WHERE name = '" . $username . " '";
-        // $res = $db2->query($sql);
-        // // Checking if any row exists with given usernameID
-        // if ($res->num_rows > 0) {
-        //     $row = mysqli_fetch_assoc($res);
-        //         // Hashing the password
-        //        $password = sha1($_POST["password"]);
-        //         // Allowing login and redirecting to main page
-        //         if ($row['password'] == $password) {
-        //             session_start();
-        //             $_SESSION["username"] = $username;
-        //             header("Location: ./index.html"); //header needs to be changed
-        //         } else {
-        //             $errors['noData']="Invalid id/password match";
-        //         }
-        // } else {
-        //     $errors['noData'] = "No record found";
-        // }
         if($username  = $USERNAME && $password = $PASSWORD){
             session_start();
                      $_SESSION["username"] = $username;
--- admin/adminPage.php
+++ admin/adminPage.php
@@ -1,26 +1,43 @@
 <?php
 require('../connect.php');
-if(isset($_POST['submitBtn']))
-{
+ini_set('display_errors',1);
+// For Posting Updates
+$state['updatePost'] = "";
+if (isset($_POST['submitBtn'])) {
+
+
     $title = $_POST['title'];
     $desc = $_POST['description'];
-    $sql = "INSERT INTO Announcements (`Title`,`Description`) 
-            VALUES (?,?);";
     $stmt = $pdo->prepare('INSERT INTO Announcements (`Title`,`Description`) VALUES (?,?);');
-    $result =  $stmt->execute([$title,$desc]);
-    $state['updatePost'] = "";
-    if($result){
-      $state['updatePost'] = "Successfully Posted";  
-    }
-    else{
+    $result =  $stmt->execute([$title, $desc]);
+
+    if ($result) {
+        $state['updatePost'] = "Successfully Posted";
+    } else {
         $state['updatePost'] = "Not able to post";
     }
+
 }
+//For Posting Scores
+$state['scorePost'] = "";
+if (isset($_POST['submitScore'])) {
+    $score = $_POST['score1'] . " - " . $_POST['score2'];
+    $winner = $_POST['score1'] > $_POST['score2'] ? $_POST['team1'] : $_POST['score1'] == $_POST['score2'] ? "Draw" : $_POST['team2'];
+        $stmt = $pdo->prepare('INSERT INTO Scores (`Game`,`Team_1`,`Team_2`,`Score`,`Winner`) VALUES (?,?,?,?,?);');
+      
+    $result = $stmt->execute([$_POST['game'],$_POST['team1'],$_POST['team2'],$score,$winner]);
+    if($result){
+        $state['scorePost'] = "Successfully Posted";  
+      }
+      else{
+          $state['scorePost'] = "Not able to post";
+      }
+
+      
+}
+
 ?>
 
-<?php
-require('../templates/header.php');
-?>
 <body>
     <h1><?php echo $state['updatePost'] ?></h1>
     <h1>Add Updates</h1>
@@ -28,15 +45,33 @@ require('../templates/header.php');
         <label for="title">
             Title
         </label>
-        <input type="text" name="title">
+        <input required type="text" name="title">
         <br>
         <label for="data">
             Description
         </label>
-        <input type="text" name="description">
+        <input required type="text" name="description">
         <br>
         <input type="submit" value="submit" name="submitBtn">
     </form>
+    <form action="" method="POST">
+        <h1><?php echo $state['scorePost'] ?></h1>
+        <label for="game">Game</label>
+        <br>
+        <input required type="text" name="game">
+        <br>
+        <label for="team_1">Team 1</label>
+        <br>
+        <input required type="text" name="team1">
+        <br>
+        <label for="team_2">Team 2</label>
+        <br>
+        <input required type="text" name="team2">
+        <br>
+        <label for="score">Score</label>
+        <br>
+        <input required type="number" name="score1">&dash;<input required type="number" name="score2">
+        <br>
+        <input type="submit" name="submitScore" value="submit">
+    </form>
 </body>
-<?php require('../templates/footer.php');
-?>
