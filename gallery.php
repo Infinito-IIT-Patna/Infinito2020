162aa1f489e4de2c8e60b774f5b0aa7f3ba3cb42
ritwizsinha <ritwizsinha0@gmail.com>
2019-08-23 09:36:10 +0530

    Made the gallery

---
+++ gallery.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./css/gallery.css?version=51">
+  <title>Infinito</title>
+</head>
+<body>
+<div id="containerr">
+  <div class="box a"> 
+    <img src="./images/gallery/01.JPG">
+  </div>
+  <div class="box b"> 
+    <img src="./images/gallery/02.JPG">
+  </div>
+  <div class="box c"> 
+    <img src="./images/gallery/03.JPG">
+  </div>
+  <div class="box d"> 
+    <img src="./images/gallery/04.JPG">
+  </div>
+  <div class="box e"> 
+    <img src="./images/gallery/06.JPG">
+  </div>
+  <div class="box f"> 
+    <img src="./images/gallery/07.JPG">
+  </div>
+  <div class="box g"> 
+    <img src="./images/gallery/08.JPG">
+  </div>
+  <div class="box h"> 
+    <img src="./images/gallery/09.JPG">
+  </div>
+  <div class="box i"> 
+    <img src="./images/gallery/10.JPG">
+  </div>
+  <div class="box j"> 
+    <img src="./images/gallery/11.JPG">
+  </div>
+  <div class="box k"> 
+    <img src="./images/gallery/12.JPG">
+  </div>
+  <div class="box l"> 
+    <img src="./images/gallery/13.JPG">
+  </div>
+  <div class="box m"> 
+    <img src="./images/gallery/14.JPG">
+  </div>
+  <div class="box n"> 
+    <img src="./images/gallery/15.JPG">
+  </div>
+  <div class="box o"> 
+    <img src="./images/gallery/16.JPG">
+  </div>
+  <div class="box p"> 
+    <img src="./images/gallery/17.JPG">
+  </div>
+  <div class="box q"> 
+    <img src="./images/gallery/18.JPG">
+  </div>
+  <div class="box r"> 
+    <img src="./images/gallery/19.JPG">
+  </div>
+  <div class="box s"> 
+    <img src="./images/gallery/20.JPG">
+  </div>
+  <div class="box t"> 
+    <img src="./images/gallery/21.JPG">
+  </div>
+  <div class="box u"> 
+    <img src="./images/gallery/22.JPG">
+  </div>
+  <div class="box v"> 
+    <img src="./images/gallery/23.JPG">
+  </div>
+  <div class="box w"> 
+    <img src="./images/gallery/24.JPG">
+  </div>
+  <div class="box x"> 
+    <img src="./images/gallery/25.JPG">
+  </div>
+  
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -61,11 +61,11 @@ require('./templates/header.php');
             <!-- SLIDE1  -->
             <li data-index="rs-280" data-transition="fade" data-slotamount="default" data-easein="default" data-easeout="default" data-masterspeed="default" data-title="Title Goes Here" data-description="">
               <!-- MAIN IMAGE -->
-              <img src="images/home/slide-1.png" alt="image" class="rev-slidebg" data-bgparallax="3" data-bgposition="center center" data-duration="20000" data-ease="Linear.easeNone" data-kenburns="on" data-no-retina="" data-offsetend="0 0" data-offsetstart="0 0" data-rotateend="0" data-rotatestart="0" data-scaleend="100" data-scalestart="140" />
+              <img src="images/home/slide-1.jpg" alt="image" class="rev-slidebg" data-bgparallax="3" data-bgposition="center center" data-duration="20000" data-ease="Linear.easeNone" data-kenburns="on" data-no-retina="" data-offsetend="0 0" data-offsetstart="0 0" data-rotateend="0" data-rotatestart="0" data-scaleend="100" data-scalestart="140" />
               <!-- LAYERS -->
 
               <!-- LAYER NR. 1 -->
-              <div class="tp-caption" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['-58','-58','0','-50']" data-width="none" data-height="none" data-whitespace="nowrap" data-transform_idle="o:1;" data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;" data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" data-mask_in="x:0px;y:[100%];" data-mask_out="x:inherit;y:inherit;" data-start="1000" data-splitin="none" data-splitout="none" data-responsive_offset="on" style="z-index: 6; white-space: nowrap;">
+              <div class="tp-caption" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['-58','-58','0','-50']" data-width="none" data-height="none" data-whitespace="nowrap" data-transform_idle="o:1;" data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;" data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" data-mask_in="x:0px;y:[100%];" data-mask_out="x:inherit;y:inherit;" data-start="1000" data-splitin="none" data-splitout="none" data-responsive_offset="on" style="z-index: 6; white-space: nowrap;   font-size: 40px">
                 <h1>HELLO WE'RE Infinito</h1>
               </div>
 
