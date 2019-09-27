eee80e435698cd91bac29a9c487e4dba97584ed6
ritwizsinha <ritwizsinha0@gmail.com>
2019-09-27 21:51:17 +0530

    beutified gallery

--- gallery.php
+++ gallery.php
@@ -1,92 +1,227 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <!-- For IE -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-    <!-- For Resposive Device -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<head>
+  <meta charset="UTF-8" />
+  <!-- For IE -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-    <title>Infinito</title>
+  <!-- For Resposive Device -->
+  <meta name="viewport" content="width=device-width, initial-scale=0.5" />
 
-    <!-- Favicon -->
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="56x56"
-      href="images/fav-icon/icon.png"
-    />
+  <title>Infinito 2k19</title>
 
-    <!-- Main style sheet -->
-    <link rel="stylesheet" type="text/css" href="css/style.css?version=51" />
-    <!-- responsive style sheet -->
-    <link rel="stylesheet" type="text/css" href="css/responsive.css?version=51" />
-    <link
-      rel="stylesheet"
-      href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
-      integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
-      crossorigin="anonymous"
-    />
-    <link rel="stylesheet" href="../vendor/owl-carousel/owl.carousel.css">
-    <link rel="stylesheet" href="owl.theme.css">
-    <link rel="stylesheet" href="css/gallery.css">
+  <!-- Favicon -->
+  <link rel="icon" type="image/png" sizes="56x56" href="images/fav-icon/icon.png" />
 
-    <!-- Fix Internet Explorer ______________________________________-->
+  <!-- Main style sheet -->
+  <link rel="stylesheet" type="text/css" href="css/style.css?version=51" />
+  <!-- responsive style sheet -->
+  <link rel="stylesheet" type="text/css" href="css/responsive.css?version=51" />
+  <link rel="stylesheet" type="text/css" href="css/countdown.css?version=51" />
+  <link rel="stylesheet" type="text/css" href="css/gallery.css?version=51" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous" />
+  <link rel="stylesheet" href="../vendor/owl-carousel/owl.carousel.css">
+  <link rel="stylesheet" href="owl.theme.css">
+  <link rel="stylesheet" href="lightbox.min.css">
+  <script src="lightbox-plus-jquery.min.js"></script>
+  <!-- Fix Internet Explorer ______________________________________-->
 
-    <!--[if lt IE 9]>
+  <!--[if lt IE 9]>
       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
       <script src="vendor/html5shiv.js"></script>
       <script src="vendor/respond.js"></script>
     <![endif]-->
-  </head>
-  <body>
+</head>
+
+<body>
   <!--
 			=============================================
 				Theme Header
 			==============================================
       -->
-<div class="bac">
-  <div class="container">
-    <a href="index.html" class="logo float-left tran4s"><img src="images/logo/logo.png" alt="Logo" /></a>
+  <div class="bac">
+    <div class="container" style="padding:10px 0">
+      <a href="index.php" class="logo float-left tran4s"><img src="images/logo/logo.png" alt="Logo" style="border-radius:100%" /></a>
 
-    <!-- ========================= Theme Feature Page Menu ======================= -->
-    <nav class="navbar float-right theme-main-menu one-page-menu">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          Menu
-          <i class="fa fa-bars" aria-hidden="true"></i>
-        </button>
-      </div>
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="navbar-collapse-1">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="./index.php">HOME</a></li>
-          <li><a href="./index.php">ABOUT</a></li>
-          <li><a href="./index.php">Events</a></li>
-          <li><a href="team/test.html">TEAM</a></li>
-          <li class="dropdown-holder">
-            <a href="./registration.php">Register</a>
-          </li>
-          <li><a href="#contact-section">CONTACT</a></li>
-        </ul>
-      </div>
-      <!-- /.navbar-collapse -->
-    </nav>
-    <!-- /.theme-feature-menu -->
+      <!-- ========================= Theme Feature Page Menu ======================= -->
+      <nav class="navbar float-right theme-main-menu one-page-menu">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            Menu
+            <i class="fa fa-bars" aria-hidden="true"></i>
+          </button>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#home">Home</a></li>
+            <li><a href="./team.php">Team</a></li>
+            <li><a href="./registration.php">Register</a></li>
 
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+      </nav>
+      <!-- /.theme-feature-menu -->
+    </div>
   </div>
-</div>
 
   <!-- /.theme-main-header -->
-    <div id="content">
-      <div class="theme-title">
-        <h2>Gallery</h2>
+  <div class="theme-title">
+    <h2>Gallery</h2>
+  </div>
+  <div class="container">
+    <div class="gallery" style="margin-top:40px">
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/01.JPG" data-lightbox="some image"><img src="images/gallery/01.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/02.JPG" data-lightbox="some image"><img src="images/gallery/02.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/03.JPG" data-lightbox="some image"><img src="images/gallery/03.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/04.JPG" data-lightbox="some image"><img src="images/gallery/04.JPG" alt=""></a>
+        </div>
       </div>
-      <div id="containerr">
-  <div class="box a"> 
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/25.JPG" data-lightbox="some image"><img src="images/gallery/25.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/06.JPG" data-lightbox="some image"><img src="images/gallery/06.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/07.JPG" data-lightbox="some image"><img src="images/gallery/07.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/08.JPG" data-lightbox="some image"><img src="images/gallery/08.JPG" alt=""></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/09.JPG" data-lightbox="some image"><img src="images/gallery/09.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/10.JPG" data-lightbox="some image"><img src="images/gallery/10.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/11.JPG" data-lightbox="some image"><img src="images/gallery/11.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/12.JPG" data-lightbox="some image"><img src="images/gallery/12.JPG" alt=""></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/13.JPG" data-lightbox="some image"><img src="images/gallery/13.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/14.JPG" data-lightbox="some image"><img src="images/gallery/14.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/15.JPG" data-lightbox="some image"><img src="images/gallery/15.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/16.JPG" data-lightbox="some image"><img src="images/gallery/16.JPG" alt=""></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/17.JPG" data-lightbox="some image"><img src="images/gallery/17.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/18.JPG" data-lightbox="some image"><img src="images/gallery/18.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/19.JPG" data-lightbox="some image"><img src="images/gallery/19.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/20.JPG" data-lightbox="some image"><img src="images/gallery/20.JPG" alt=""></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/21.JPG" data-lightbox="some image"><img src="images/gallery/21.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/22.JPG" data-lightbox="some image"><img src="images/gallery/22.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/23.JPG" data-lightbox="some image"><img src="images/gallery/23.JPG" alt=""></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="images/gallery/24.JPG" data-lightbox="some image"><img src="images/gallery/24.JPG" alt=""></a>
+        </div>
+      </div>
+    </div>
+    <!-- <a href="images/gallery/12.JPG"><img src="images/gallery/12.JPG" alt=""></a>
+    <a href="images/gallery/13.JPG"><img src="images/gallery/13.JPG" alt=""></a>
+    <a href="images/gallery/11.JPG"><img src="images/gallery/11.JPG" alt=""></a>
+    <a href="images/gallery/11.JPG"><img src="images/gallery/11.JPG" alt=""></a>
+    <a href="images/gallery/11.JPG"><img src="images/gallery/11.JPG" alt=""></a>
+    <a href="images/gallery/11.JPG"><img src="images/gallery/11.JPG" alt=""></a>
+    <a href="images/gallery/11.JPG"><img src="images/gallery/11.JPG" alt=""></a>
+    <a href="images/gallery/11.JPG"><img src="images/gallery/11.JPG" alt=""></a>
+    <a href="images/gallery/11.JPG"><img src="images/gallery/11.JPG" alt=""></a>
+    <a href="images/gallery/11.JPG"><img src="images/gallery/11.JPG" alt=""></a>
+    <a href="images/gallery/11.JPG"><img src="images/gallery/11.JPG" alt=""></a>
+    <a href="images/gallery/11.JPG"><img src="images/gallery/11.JPG" alt=""></a>
+    <a href="images/gallery/11.JPG"><img src="images/gallery/11.JPG" alt=""></a>
+    <a href="images/gallery/11.JPG"><img src="images/gallery/11.JPG" alt=""></a>
+    <a href="images/gallery/11.JPG"><img src="images/gallery/11.JPG" alt=""></a> -->
+
+  </div>
+  </div>
+
+  <!--
+			=====================================================
+				Footer
+			=====================================================
+			-->
+  <footer>
+    <div class="container">
+      <a href="index.php" class="logo"><img src="images/logo/logo.png" alt="Logo" style="border-radius:100%" /></a>
+
+      <ul>
+        <li>
+          <a href="https://www.facebook.com/InfinitoIITPatna/" target="_blank" class="tran3s round-border"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/infinito_iitp/" target="_blank" class="tran3s round-border"><i class="fab fa-instagram"></i></a>
+        </li>
+
+      </ul>
+    </div>
+  </footer>
+
+
+  <!-- =============================================
+				Loading Transition
+			============================================== -->
+  <div id="loader-wrapper">
+    <div id="preloader_1">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+
+  <!-- Scroll Top Button -->
+  <button class="scroll-top tran3s p-color-bg">
+    <i class="fas fa-long-arrow-alt-up" aria-hidden="true"></i>
+  </button>
+  <?php
+  require('./templates/footer.php')
+  ?>
+  <!-- <div class="box a"> 
     <img src="./images/gallery/01.JPG">
   </div>
   <div class="box b"> 
@@ -158,36 +293,4 @@
   <div class="box x"> 
     <img src="./images/gallery/25.JPG">
   </div>
-  
-</div>
-    </div>
-  
-
-
-
-
-  <!--
-			=====================================================
-				Footer
-			=====================================================
-			-->
-  <!-- =============================================
-				Loading Transition
-			============================================== -->
-  <div id="loader-wrapper">
-    <div id="preloader_1">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-  </div>
-
-  <!-- Scroll Top Button -->
-  <button class="scroll-top tran3s p-color-bg">
-    <i class="fas fa-long-arrow-alt-up" aria-hidden="true"></i>
-  </button>
-  <?php
-  require('./templates/footer.php')
-  ?>
+   -->
