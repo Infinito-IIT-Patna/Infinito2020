aa7e05ac9750b0eefad725d3ead26e967e448ad5
Subhang23 <vsubhang4@gmail.com>
2019-09-30 22:18:17 +0530

    some changes

--- team.php
+++ team.php
@@ -2,47 +2,49 @@
 require('./templates/header.php');
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <!-- For IE -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+  <!-- For Resposive Device -->
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <title>Infinito 2k19</title>
+
+  <!-- Favicon -->
+  <link rel="icon" type="image/png" sizes="56x56" href="images/fav-icon/icon.png" />
+
+  <!-- Main style sheet -->
+  <link rel="stylesheet" type="text/css" href="css/style.css?version=51" />
+  <!-- responsive style sheet -->
+  <link rel="stylesheet" type="text/css" href="css/responsive.css?version=51" />
+  <link rel="stylesheet" type="text/css" href="css/countdown.css?version=51" />
+  <link rel="stylesheet" href="css/team.css">
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous" />
+  <link rel="stylesheet" href="../vendor/owl-carousel/owl.carousel.css">
+  <link rel="stylesheet" href="owl.theme.css">
+
+  <!-- Fix Internet Explorer ______________________________________-->
+
+  <!--[if lt IE 9]>
+      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+      <script src="vendor/html5shiv.js"></script>
+      <script src="vendor/respond.js"></script>
+    <![endif]-->
+</head>
+
 <body>
-<div class="bac">
-    <div class="container" style="padding:10px 0">
-      <a href="index.php" class="logo float-left tran4s"><img src="images/logo/logo.png" alt="Logo" style="border-radius:100%" /></a>
-
-      <!-- ========================= Theme Feature Page Menu ======================= -->
-      <nav class="navbar float-right theme-main-menu one-page-menu">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            Menu
-            <i class="fa fa-bars" aria-hidden="true"></i>
-          </button>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#home">Home</a></li>
-            <li><a href="./team.php">Team</a></li>
-            <li><a href="./gallery.php">Gallery</a></li>
-            <li class="dropdown-holder"><a>Register</a>
-                <ul class="sub-menu">
-                  <li><a href="./registration.php">Register Team</a></li>
-                  <li><a href="./registerplayer.php">Register Player</a></li>
-                </ul>
-              </li>
-
-          </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-      </nav>
-      <!-- /.theme-feature-menu -->
-    </div>
-  </div>
 
   <div id="content">
     <div class="theme-title">
       <h2>Our Team</h2>
     </div>
-    <div id="team-part">
+<!--     
       <div class="box">
         <div class="card">
           <div class="imgBx">
@@ -127,7 +129,122 @@ require('./templates/header.php');
           <h2>Hospitality<br><span>Neeraj Meena</span></h2>
           </div>
         </div>
+      </div> -->
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <div class="card">
+          <div class="imgBx">
+            <img src="team/Coordinator.jpg" alt="">
+          </div>
+          <div class="details">
+            <h2>Overall Coordinator<br><span>Rahul Pandey</span></h2>
+          </div>
+        </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <div class="card">
+          <div class="imgBx">
+            <img src="team/FestConvener.jpg" alt="">
+          </div>
+          <div class="details">
+            <h2>Fest Convener<br><span>Akshay Rajput</span></h2>
+          </div>
+        </div>
+          </div>
       </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <div class="card">
+          <div class="imgBx">
+            <img src="team/MarketingNSponsorship.jpeg" alt="">
+          </div>
+          <div class="details">
+          <h2>Marketing and Sponsorship<br><span>Deep Roshan</span></h2>
+          </div>
+        </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <div class="card">
+          <div class="imgBx">
+            <img src="team/Mpr.jpg" alt="">
+          </div>
+          <div class="details">
+          <h2>Media and Public Relations<br><span>Rupesh Neekhra</span></h2>
+
+          </div>
+        </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <div class="card">
+          <div class="imgBx">
+            <img src="team/RegistrationNSecurity.jpg" alt="">
+          </div>
+          <div class="details">
+          <h2>Registration and Security<br><span>Neeraj Paraliya</span></h2>
+
+          </div>
+        </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <div class="card">
+          <div class="imgBx">
+            <img src="team/CreativeNDesign.jpg" alt="">
+          </div>
+          <div class="details">
+          <h2>Creatives  and Design<br><span>Nayan Thulkar</span></h2>
+
+          </div>
+        </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <div class="card">
+          <div class="imgBx">
+            <img src="team/Organizing.jpg" alt="">
+          </div>
+          <div class="details">
+          <h2>Organizing<br><span>Vikram Patel</span></h2>
+          </div>
+        </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+          <div class="card">
+          <div class="imgBx">
+            <img src="team/Organizing2.jpg" alt="">
+          </div>
+          <div class="details">
+          <h2>Organizing<br><span>Amar Raj</span></h2>
+          </div>
+        </div>
+          </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <div class="card">
+          <div class="imgBx">
+            <img src="team/WebNApp.jpeg" alt="">
+          </div>
+          <div class="details">
+          <h2>Website Developer<br><span>Amit Priyankar</span></h2>
+          </div>
+        </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <div class="card">
+          <div class="imgBx">
+            <img src="team/Hospitality.png" alt="">
+          </div>
+          <div class="details">
+          <h2>Hospitality<br><span>Neeraj Meena</span></h2>
+          </div>
+        </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+          
+          </div>
+      </div>
+      <!-- Div ends here -->
     </div>
   </div>
 
