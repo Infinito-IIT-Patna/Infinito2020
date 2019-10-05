90f3ec5bf65359aad944050a6c8468271ec28fd1
Subhang23 <vsubhang4@gmail.com>
2019-10-06 01:51:37 +0530

    fixing bugs

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@ date_default_timezone_set('Asia/Calcutta');
 $host = 'localhost';
 $db   = 'infinitoAdmin';
 $user = 'root';
-$pass = '';
+$pass = 'password';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
