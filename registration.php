18beb4e4a96fd262e8451c14045e1d82c5c3a784
StrixROX <40167899+StrixROX@users.noreply.github.com>
2020-12-25 03:38:09 +0530

    Fixed formatting across all custom files

--- registration.php
+++ registration.php
@@ -2,173 +2,179 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <!-- For IE -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta charset="UTF-8" />
+    <!-- For IE -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-  <!-- For Resposive Device -->
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- For Resposive Device -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <title>Infinito 2k19</title>
+    <title>Infinito 2k19</title>
 
-  <!-- Favicon -->
-  <link rel="icon" type="image/png" sizes="56x56" href="images/logo/logo.png" />
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="56x56" href="images/logo/logo.png" />
 
-  <!-- Main style sheet -->
-  <link rel="stylesheet" type="text/css" href="css/style.css?version=51" />
-  <!-- responsive style sheet -->
-  <link rel="stylesheet" type="text/css" href="css/responsive.css?version=51" />
-  <link rel="stylesheet" type="text/css" href="css/countdown.css?version=51" />
+    <!-- Main style sheet -->
+    <link rel="stylesheet" type="text/css" href="css/style.css?version=51" />
+    <!-- responsive style sheet -->
+    <link rel="stylesheet" type="text/css" href="css/responsive.css?version=51" />
+    <link rel="stylesheet" type="text/css" href="css/countdown.css?version=51" />
 
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous" />
+    <link rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
+        crossorigin="anonymous"
+    />
 
-  <!-- Fix Internet Explorer ______________________________________-->
+    <!-- Fix Internet Explorer ______________________________________-->
 
-  <!--[if lt IE 9]>
-      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-      <script src="vendor/html5shiv.js"></script>
-      <script src="vendor/respond.js"></script>
-    <![endif]-->
+    <!--[if lt IE 9]>
+            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+            <script src="vendor/html5shiv.js"></script>
+            <script src="vendor/respond.js"></script>
+        <![endif]-->
 </head>
 
 <body>
-  <!--
-			=============================================
-				Theme Header
-			==============================================
-      -->
-  <div class="bac">
-    <div class="container" style="padding:10px 0">
-      <a href="index.php" class="logo float-left tran4s"><img src="images/logo/logo.png" alt="Logo" style="border-radius:100%" /></a>
+    <!--
+	=============================================
+		Theme Header
+	==============================================
+    -->
+    <div class="bac">
+        <div class="container" style="padding:10px 0">
+            <a href="index.php" class="logo float-left tran4s"><img src="images/logo/logo.png" alt="Logo" style="border-radius:100%" /></a>
 
-      <!-- ========================= Theme Feature Page Menu ======================= -->
-      <nav class="navbar float-right theme-main-menu one-page-menu">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            Menu
-            <i class="fa fa-bars" aria-hidden="true"></i>
-          </button>
+            <!-- ========================= Theme Feature Page Menu ======================= -->
+            <nav class="navbar float-right theme-main-menu one-page-menu">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        Menu
+                        <i class="fa fa-bars" aria-hidden="true"></i>
+                    </button>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="./index.php">Home</a></li>
+                        <li><a href="./team.php">Team</a></li>
+                        <li><a href="./gallery.php">Gallery</a></li>
+                        <li class="active"><a href="./registration.php">Register</a></li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </nav>
+            <!-- /.theme-feature-menu -->
         </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li><a href="./index.php">Home</a></li>
-            <li><a href="./team.php">Team</a></li>
-            <li><a href="./gallery.php">Gallery</a></li>
-            <li class="active"><a href="./registration.php">Register</a></li>
-          </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-      </nav>
-      <!-- /.theme-feature-menu -->
-    </div>
-  </div>
-
-  <!-- /.theme-main-header -->
-  <div class="container">
-    <div id="register" style="height:100vh">
-      <div class="theme-title">
-        <h2>Register</h2>
-      </div>
-      <div class="list-group" style="margin-top:40px">
-        <a href="#" class="list-group-item list-group-item-action active">
-          Steps For Registration
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          Register yourself individually. You will be receiving an Infinito ID over the registered email.
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          The Infinito ID registers you in Infinito as a spectator or participant.
-         </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          The team captain shall register the team along with the respective infinito IDs of the players.
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          If participating in individual events, please register yourself as captain and enter the number of players as zero.
-        </a>
-        <a href="#" class="list-group-item list-group-item-action ">
-          If registering for team events, please enter the total number of players, excluding the captain, the captain is registered automatically with the team.
-        </a>
-        <a href="#" class="list-group-item list-group-item-action ">
-          The registration fee will be accepted after complete verification on the day of the event.
-        </a>
-        <a href="#" class="list-group-item list-group-item-action ">
-            Registration fee is Rs. 200 for Infinito 2k19.
-        </a>
-        <a href="#" class="list-group-item list-group-item-action ">
-          In order to participate in more than one events, you should be registered with your Infinito ID in all of the events. Registration fee of Rs 200 has to be paid seperately for each of these events.
-        </a>
-      </div>
-      <div class="row" style="margin-top:3%;" >
-        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6" >
-          <a class="tp-caption">
-            <a href="./registerPlayer.php" class="score-btn project-button hvr-bounce-to-right" style="padding:10px 30px;font-size:16px;border: #d8545d 2px solid;">Player / Spectator</a>
-          </a>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6">
-        <a class="tp-caption">
-            <a href="./registerTeam.php" class="score-btn project-button hvr-bounce-to-right" style="padding:10px 30px;font-size:16px;border: #d8545d 2px solid;" >Team Registration</a>
-          </a>
-        </div>
-      </div>
     </div>
 
-  </div>
-
-  <!--
-			=====================================================
-				Footer
-			=====================================================
-			-->
-  <footer>
+    <!-- /.theme-main-header -->
     <div class="container">
-      <a href="index.php" class="logo"><img src="images/logo/logo.png" alt="Logo" style="border-radius:100%" /></a>
+        <div id="register" style="height:100vh">
+            <div class="theme-title">
+                <h2>Register</h2>
+            </div>
+            <div class="list-group" style="margin-top:40px">
+                <a href="#" class="list-group-item list-group-item-action active">
+                    Steps For Registration
+                </a>
+                <a href="#" class="list-group-item list-group-item-action">
+                    Register yourself individually. You will be receiving an Infinito ID over the registered email.
+                </a>
+                <a href="#" class="list-group-item list-group-item-action">
+                    The Infinito ID registers you in Infinito as a spectator or participant.
+                 </a>
+                <a href="#" class="list-group-item list-group-item-action">
+                    The team captain shall register the team along with the respective infinito IDs of the players.
+                </a>
+                <a href="#" class="list-group-item list-group-item-action">
+                    If participating in individual events, please register yourself as captain and enter the number of players as zero.
+                </a>
+                <a href="#" class="list-group-item list-group-item-action ">
+                    If registering for team events, please enter the total number of players, excluding the captain, the captain is registered automatically with the team.
+                </a>
+                <a href="#" class="list-group-item list-group-item-action ">
+                    The registration fee will be accepted after complete verification on the day of the event.
+                </a>
+                <a href="#" class="list-group-item list-group-item-action ">
+                        Registration fee is Rs. 200 for Infinito 2k19.
+                </a>
+                <a href="#" class="list-group-item list-group-item-action ">
+                    In order to participate in more than one events, you should be registered with your Infinito ID in all of the events. Registration fee of Rs 200 has to be paid seperately for each of these events.
+                </a>
+            </div>
+            <div class="row" style="margin-top:3%;" >
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6" >
+                    <a class="tp-caption">
+                        <a href="./registerPlayer.php" class="score-btn project-button hvr-bounce-to-right" style="padding:10px 30px;font-size:16px;border: #d8545d 2px solid;">Player / Spectator</a>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6">
+                    <a class="tp-caption">
+                        <a href="./registerTeam.php" class="score-btn project-button hvr-bounce-to-right" style="padding:10px 30px;font-size:16px;border: #d8545d 2px solid;" >Team Registration</a>
+                    </a>
+                </div>
+            </div>
+        </div>
 
-      <ul>
-        <li>
-          <a href="https://www.facebook.com/InfinitoIITPatna/" target="_blank" class="tran3s round-border"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/infinito_iitp/" target="_blank" class="tran3s round-border"><i class="fab fa-instagram"></i></a>
-        </li>
-
-      </ul>
     </div>
-  </footer>
+
+    <!--
+	=====================================================
+		Footer
+	=====================================================
+	-->
+    <footer>
+        <div class="container">
+            <a href="index.php" class="logo"><img src="images/logo/logo.png" alt="Logo" style="border-radius:100%" /></a>
+
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/InfinitoIITPatna/" target="_blank" class="tran3s round-border"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/infinito_iitp/" target="_blank" class="tran3s round-border"><i class="fab fa-instagram"></i></a>
+                </li>
+
+            </ul>
+        </div>
+    </footer>
 
 
-  <!-- =============================================
-				Loading Transition
-			============================================== -->
-  <div id="loader-wrapper">
-    <div id="preloader_1">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
+    <!--
+    =============================================
+		Loading Transition
+    ==============================================
+    -->
+    <div id="loader-wrapper">
+        <div id="preloader_1">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
     </div>
-  </div>
 
 
-  <!-- Scroll Top Button -->
-  <button class="scroll-top tran3s p-color-bg">
-    <i class="fas fa-long-arrow-alt-up" aria-hidden="true"></i>
-  </button>
-  <!-- Js File_________________________________ -->
+    <!-- Scroll Top Button -->
+    <button class="scroll-top tran3s p-color-bg">
+        <i class="fas fa-long-arrow-alt-up" aria-hidden="true"></i>
+    </button>
+    <!-- Js File_________________________________ -->
 
-  <!-- j Query -->
-  <script type="text/javascript" src="vendor/jquery.2.2.3.min.js"></script>
-  <!-- Bootstrap JS -->
-  <script type="text/javascript" src="vendor/bootstrap/bootstrap.min.js"></script>
+    <!-- j Query -->
+    <script type="text/javascript" src="vendor/jquery.2.2.3.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script type="text/javascript" src="vendor/bootstrap/bootstrap.min.js"></script>
 
-  <!-- Theme js -->
-  <script type="text/javascript" src="js/theme.js"></script>
+    <!-- Theme js -->
+    <script type="text/javascript" src="js/theme.js"></script>
 
-  </div>
-  <!-- /.main-page-wrapper -->
+    </div>
+    <!-- /.main-page-wrapper -->
 </body>
 
 </html>
