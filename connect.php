2a6652bd4b93d542f5b0f5f38a0c1688a4ca5761
Aditya Kumar Singh <adityakumarsingh1403@gmail.com>
2020-12-26 17:43:13 +0530

    Countdown started working

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@ date_default_timezone_set('Asia/Calcutta');
 $host = 'localhost';
 $db   = 'infinitoAdmin';
 $user = 'root';
-$pass = '12345678';
+$pass = 'DBPASSWORD';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
