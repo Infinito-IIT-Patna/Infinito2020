12ac161565ac5c8f47b10c156b6e7b800664cf65
Rishikesh Devanathan <rishi.devanathan@gmail.com>
2020-12-31 15:27:16 +0530

    Final Changes

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ date_default_timezone_set('Asia/Calcutta');
 $host = 'localhost';
 $db   = 'infinitoAdmin';
 $user = 'root';
-$pass = 'infinito123';
+$pass = DBPASSWORD;
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
--- index.php
+++ index.php
@@ -637,7 +637,7 @@
 		Loading Transition
     ==============================================
     -->
-    <div id="loader-wrapper" style="">
+    <div id="loader-wrapper">
         <div id="preloader_1">
             <span></span>
             <span></span>
