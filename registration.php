65ed7609914f1266e1935b984d1ef917db8d39d7
debarghya472 <debarghya_1901me19@iitp.ac.in>
2021-01-01 14:52:52 +0530

    fix

--- registration.php
+++ registration.php
@@ -90,14 +90,15 @@
                     For registering for any event, go to the event section, Select the appropriate link and register using Infinito ID provided
                 </a>
                 <a href="#" class="list-group-item list-group-item-action ">
-                    The registration fee will be accepted after complete verification on the day of the event.
+                        Event Registration fee is Rs. 20 for each event.
                 </a>
                 <a href="#" class="list-group-item list-group-item-action ">
-                        Registration fee is Rs. 200 for Infinito 2k20.
+                    In order to participate in more than one events, you should be registered with your Infinito ID in all of the events. Events Registration fee of Rs 20 has to be paid seperately for each of these events.
                 </a>
-                <a href="#" class="list-group-item list-group-item-action ">
-                    In order to participate in more than one events, you should be registered with your Infinito ID in all of the events. Registration fee of Rs 200 has to be paid seperately for each of these events.
+                <a href="https://drive.google.com/file/d/1iWYbBkCWc-DQyNbE9atzdbtsI_mgQ9Hj/view?usp=drivesdk" style="color: #d8545d;" class="list-group-item list-group-item-action" >
+                   Pay the registration fees through Qr code or UPI ID. Click here for Payment
                 </a>
+
             </div>
             <div class="row" style="margin-top:3%;" >
                 <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6" >
