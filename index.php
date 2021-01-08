8132b924b6ab07754dcb53fe065758a92f072085
supershivam13 <shivam_1901cs55@iitp.ac.in>
2021-01-08 14:50:49 +0530

    minor bugs corrected

--- index.php
+++ index.php
@@ -8,80 +8,6 @@
     require('./templates/header.php');
     ?>
      
-
-     <style>
-* {box-sizing: border-box;}
-body {font-family: Verdana, sans-serif;}
-.mySlides {display: none;}
-img {vertical-align: middle;}
-
-/* Slideshow container */
-.slideshow-container {
-  max-width: 1000px;
-  position: relative;
-  margin: auto;
-}
-
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-}
-
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-
-/* The dots/bullets/indicators */
-.dot {
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-
-
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 3.0s;
-  animation-name: fade;
-  animation-duration: 3.0s;
-}
-
-@-webkit-keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
-
-@keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
-
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 300px) {
-  .text {font-size: 11px}
-}
-
-</style>
-
-
-
 </head>
 
 
@@ -516,7 +442,7 @@ img {vertical-align: middle;}
 <div  class="container">
     <div class="container" >
         <div class="theme-title">
-            <h2>Guest Lectures</h2>
+            <h2>Guest Lectures  </h2>
         </div>
 
     </div>
