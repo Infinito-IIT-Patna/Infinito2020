03206340bdd85f031fd941cc0ceff6a85792641a
Kartikay Gupta <kartikay.gupta2002@gmail.com>
2022-10-19 22:36:18 +0530

    Chnages

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
 
 $servername="localhost";
 //$username= "iitpsports";
-$username= "root";
+$username= "id19708136_infinito2k22_user";
 //$password="infi5-2021";
-$password="Ayushpal@01";
+$password="Celesta@2k22";
 $database="infinito2k22";
 $conn=mysqli_connect($servername ,$username ,$password ,$database);
 ?>
